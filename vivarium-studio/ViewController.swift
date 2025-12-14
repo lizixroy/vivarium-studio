4c62c2d5dfa16ae9fa654002a1a37891a0b01e4d
Roy Li <roy.li.zixuan@gmail.com>
2025-12-14 10:06:12 +0900

    initial commit

---
+++ vivarium-studio/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  vivarium-studio
+//
+//  Created by Roy Li on 12/14/25.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ vivarium-studioUITests/vivarium_studioUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  vivarium_studioUITestsLaunchTests.swift
+//  vivarium-studioUITests
+//
+//  Created by Roy Li on 12/14/25.
+//
+
+import XCTest
+
+final class vivarium_studioUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
